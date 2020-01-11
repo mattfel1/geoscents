c1555fb011debd8530cf66849c0a35fba0d3b6c4
mattfel <feldman.matthew1@gmail.com>
2020-01-11 09:51:13 -0800

    add anomialies

--- server/app.js
+++ server/app.js
@@ -37,6 +37,10 @@ app.get('/', (req, res, next) => {
 //  3) Take top 1000 cities
 //  4) xlsx -> csv (https://www.zamzar.com/convert/xlsx-to-csv/)
 //  5) csv -> json (https://csvjson.com/csv2json)
+app.get('/resources/*.html', (req, res, next) => {
+    const wildcard = req.params['0'];
+	res.sendFile(path.join(__dirname, '..', 'resources/' + wildcard + '.html'));
+});
 app.get('/resources/*.png', (req, res, next) => {
     const wildcard = req.params['0'];
 	res.sendFile(path.join(__dirname, '..', 'resources/' + wildcard + '.png'));
@@ -81,6 +85,7 @@ const rooms = {
 };
 var playerRooms = new Map();
 
+const WELCOME_MESSAGE3 = '[ <b>ANOMALIES</b> ] <a href="http://geoscents.net/resources/anomalies.html">Click here</a> for info about geopolitical anomalies in the game!<br>';
 const WELCOME_MESSAGE2 = '[ <b>GREETING</b> ] Welcome to Geoscents, an online multiplayer world geography game! ' +
                           'This is an attempt at recreating the similarly-named game from the mid 2000s, Geosense (geosense.net), which is no longer available. ' +
                           'If you are enjoying this game, please share it with a friend!  If you really love it, consider donating at the bottom of the page to help keep the server ' +
@@ -103,6 +108,7 @@ io.on('connection', (socket) => {
       socket.emit("update custom messages", CONSTANTS.LOBBY, REFERENCE3, 10);
       socket.emit("update custom messages", CONSTANTS.LOBBY, REFERENCE2, 10);
       socket.emit("update custom messages", CONSTANTS.LOBBY, REFERENCE1, 10);
+	  socket.emit("update messages", CONSTANTS.LOBBY, WELCOME_MESSAGE3);
 	  socket.emit("update messages", CONSTANTS.LOBBY, WELCOME_MESSAGE2);
 	  socket.emit("update messages", CONSTANTS.LOBBY, WELCOME_MESSAGE1);
 	  //socket.emit("update messages", CONSTANTS.LOBBY, WELCOME_MESSAGE2);
