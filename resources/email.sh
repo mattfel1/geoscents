2ce667c49fbd55caf7499e4d6fb0c32bd68e71b9
mattfel <feldman.matthew1@gmail.com>
2020-02-04 11:53:38 -0800

    add email script

---
+++ resources/email.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ip=`tail -1 /scratch/feedback.log | sed "s/.*Message passed by ::ffff://g" | sed "s/ .*//g"`
+name=`tail -1 /scratch/feedback.log | sed "s/.*$ip //g" | sed "s/:.*//g"`
+echo "Subject: GeoScents Feedback from $name" > /scratch/email.txt
+echo "" >> /scratch/email.txt
+tail -1 /scratch/feedback.log >> /scratch/email.txt
+echo "" >> /scratch/email.txt
+wget -O lookup ip-api.com/json/$ip
+cat lookup >> /scratch/email.txt
+
+cat /scratch/email.txt | sendmail feldman.matthew1@gmail.com
+
