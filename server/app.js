a8c7632f6e1091ec97a0b7215ec9a8567dc2340a
mattfel <feldman.matthew1@gmail.com>
2020-03-31 15:33:44 -0700

    update

--- server/app.js
+++ server/app.js
@@ -89,18 +89,12 @@ const rooms = {
 };
 var playerRooms = new Map();
 
-const ABOUT_MESSAGE = '[ <b>ABOUT</b> ] <a href="http://geoscents.net/resources/anomalies.html" target="_blank">Click here</a> for info about geopolitical anomalies and details about the game!<br>';
-const PRIVACY_POLICY = '[ <b>PRIVACY POLICY</b> ] We do not use cookies to track you after you leave this page.' +
-                       ' For educational purposes, we store your public IP address and your game history securely on our server. The IP addresses are replaced with country and region names, and then this data is published to <a href="http://geoscents.net/visualization/index.html">http://geoscents.net/visualization/index.html</a>.' +
-                       ' This data is not used for any commercial purposes.  If you would like to opt out, simply type /private into the chat box.  If you would like to opt in, type /public into the chat box.<br>'
+const ABOUT_MESSAGE = '[ <b>ABOUT</b> ] <a href="http://geoscents.net/resources/anomalies.html" target="_blank">Click here</a> for info about geopolitical anomalies, details about the game, the privacy policy, and other info!<br>';
 const WELCOME_MESSAGE1 = '[ <b>GREETING</b> ] Welcome to Geoscents, an online multiplayer world geography game! ' +
                           'This is an attempt at recreating the similarly-named game from the mid 2000s, Geosense (geosense.net), which is no longer available. ' +
                           '<br>If you have feedback, simply shout it directly into this chat box, starting with the /feedback.' +
                           'If you are enjoying this game, please share it with a friend!  If you really love it, consider donating at the bottom of the page to help keep the server ' +
                           'running!<br>';
-const REFERENCE1 = '[ <b>REFERENCES</b> ] Terrain map rendering provided by by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.  Satellite map rendering provided by Google Tiles.  All maps generated using python cartopy 0.17.0.  '
-const REFERENCE2 = 'This game uses the most populous and important cities from the database at <a href="https://simplemaps.com/data/world-cities">https://simplemaps.com/data/world-cities</a>.  '
-const REFERENCE3 = 'The jingle at the start of the game was composed and recorded by Marc Ryan Feldman.<br>'
 
 //const WELCOME_MESSAGE2 = '[ <b>UPDATE 1/6/2019</b> ] The yearly records are supposed to reset on 1/1/2020, but because of a mistake with cron, they were erroneously reset again on 1/6/2020.  Sorry!<br>'
 
@@ -124,10 +118,10 @@ io.on('connection', (socket) => {
       io.sockets.emit('update counts', rooms[CONSTANTS.LOBBY].playerCount(),rooms[CONSTANTS.WORLD].playerCount(),rooms[CONSTANTS.US].playerCount(),rooms[CONSTANTS.EURO].playerCount(),rooms[CONSTANTS.AFRICA].playerCount(),rooms[CONSTANTS.SAMERICA].playerCount(),rooms[CONSTANTS.ASIA].playerCount(),rooms[CONSTANTS.OCEANIA].playerCount(),rooms[CONSTANTS.MISC].playerCount());
       helpers.logHistogram(rooms);
       helpers.log("User connected    " + socket.handshake.address);
-      socket.emit("update custom messages", CONSTANTS.LOBBY, REFERENCE3, 10);
-      socket.emit("update custom messages", CONSTANTS.LOBBY, REFERENCE2, 10);
-      socket.emit("update custom messages", CONSTANTS.LOBBY, REFERENCE1, 10);
-      socket.emit("update custom messages", CONSTANTS.LOBBY, PRIVACY_POLICY, 10);
+      // socket.emit("update custom messages", CONSTANTS.LOBBY, REFERENCE3, 10);
+      // socket.emit("update custom messages", CONSTANTS.LOBBY, REFERENCE2, 10);
+      // socket.emit("update custom messages", CONSTANTS.LOBBY, REFERENCE1, 10);
+      // socket.emit("update custom messages", CONSTANTS.LOBBY, PRIVACY_POLICY, 10);
 	  socket.emit("update custom messages", CONSTANTS.LOBBY, ABOUT_MESSAGE, 10);
 	  // socket.emit("update messages", CONSTANTS.LOBBY, WELCOME_MESSAGE2);
 	  socket.emit("update messages", CONSTANTS.LOBBY, WELCOME_MESSAGE1);
