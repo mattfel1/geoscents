cadf987283e5eef5e37656b9463a427243a63926
mattfel <feldman.matthew1@gmail.com>
2020-03-30 21:07:14 -0700

    surgical cp

--- startdaily.sh
+++ startdaily.sh
@@ -4,5 +4,10 @@ pkill -f npm
 pkill -f node
 bash /root/update.sh
 bash /root/npmstart.sh &
-cp /home/mattfel/visualization/* /var/www/html/visualization/
+
+cp /home/mattfel/visualization/*.js /var/www/html/visualization/
+cp /home/mattfel/visualization/*.css /var/www/html/visualization/
+cp /home/mattfel/visualization/*.html /var/www/html/visualization/
+cp /home/mattfel/visualization/*.jpg /var/www/html/visualization/
+cp /home/mattfel/visualization/*.gif /var/www/html/visualization/
 
