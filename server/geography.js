8bd3793c971259df5fc955e6a9113aed46bc53e1
mattfel <feldman.matthew1@gmail.com>
2020-04-05 20:20:34 -0700

    tweak timing

--- server/geography.js
+++ server/geography.js
@@ -197,7 +197,7 @@ const score = (room, geoDist, mercDist, timeBonus) => {
     // return distGaussian * timeLogistic;
     // Geo distance based score
     const minTimePortion = 1/3;
-    const timeCushion = 1.7;
+    const timeCushion = 1.55;
     const slope = (1 - minTimePortion) / (timeCushion - 10);
     const invTime = CONSTANTS.GUESS_DURATION - timeBonus;
     const timeLine = slope * (invTime - timeCushion) + 1;
