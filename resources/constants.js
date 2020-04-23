d76713eaed83fbf253ff41a6144ceef8398e2879
mattfel <feldman.matthew1@gmail.com>
2020-04-22 21:46:31 -0700

    less time between rounds

--- resources/constants.js
+++ resources/constants.js
@@ -54,7 +54,7 @@ module.exports = {
 
     // FSM
     REVEAL_STATE: 'reveal',
-    REVEAL_DURATION: 6,
+    REVEAL_DURATION: 5,
     GUESS_STATE: 'guess',
     GUESS_DURATION: 10,
     SETUP_STATE: 'setup',
