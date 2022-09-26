ffbcc19248c85bcfd0111ee5266b94ac82c9dcf8
mattfel1 <mattfel@pm.me>
2022-09-25 18:42:42 -0700

    happy lil fixes

--- scripts/email.sh
+++ scripts/email.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source /root/.bashrc
+
 ip=`tail -1 /scratch/feedback.log | sed "s/.*Message passed by ::ffff://g" | sed "s/ .*//g"`
 name=`tail -1 /scratch/feedback.log | sed "s/.*$ip //g" | sed "s/:.*//g"`
 echo "Subject: GeoScents Feedback from $name" > /scratch/email.txt
@@ -9,5 +11,8 @@ echo "" >> /scratch/email.txt
 wget -O lookup ip-api.com/json/$ip
 cat lookup >> /scratch/email.txt
 
-cat /scratch/email.txt | sendmail mattfel@protonmail.com
+# smpt doesn't work with any of the major services anymore..
+#cat /scratch/email.txt | sendmail mattfel@protonmail.com
 
+# Use pushover app:
+push "`cat /scratch/email.txt`"
