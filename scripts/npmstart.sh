c5dcfa6db2cddebc20aab6c04e4deeda8e9e0745
mattfel1 <mattfel@pm.me>
2022-10-01 21:41:01 -0700

    happy lil fixes

--- scripts/npmstart.sh
+++ scripts/npmstart.sh
@@ -6,7 +6,7 @@ export PATH=/usr/local/bin/:$PATH
 touch /root/npm_log
 rm /root/npm_log
 cd /var/www/html
-npm update
+# npm update DO NOT DO THIS OR ELSE WEBPACK GETS MESSED UP
 webpack
 pm2 start --no-daemon 2>&1 | tee /root/npm_log
 
